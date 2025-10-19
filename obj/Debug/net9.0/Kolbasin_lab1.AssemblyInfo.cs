17ab895adda1a6ec2e5c357491d9e72e1e608eea
Arseny <arsenykolbasin@gmail.com>
2025-10-19 13:23:46 +0300

    change views

--- obj/Debug/net9.0/Kolbasin_lab1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Kolbasin_lab1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kolbasin_lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f85932c812213f7fb93152971fcb868570c666b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kolbasin_lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kolbasin_lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
